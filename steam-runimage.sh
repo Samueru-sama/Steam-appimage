f58818fd2fe5f9ad23a74ddfc7273ff50b2c4fa1
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-05-11 12:07:55 -0400

    duplicated package

--- steam-runimage.sh
+++ steam-runimage.sh
@@ -14,9 +14,10 @@ run_install() {
 
 	INSTALL_PKGS=(
 		steam egl-wayland vulkan-radeon vulkan-intel vulkan-nouveau
-		lib32-libpipewire libpipewire pipewire lib32-libpipewire libpulse lib32-libpulse 
-		vkd3d lib32-vkd3d vulkan-mesa-layers lib32-vulkan-mesa-layers freetype2 lib32-freetype2 
-		fuse2 wget yad mangohud lib32-mangohud gamescope gamemode zenity-gtk3 steam-screensaver-fix
+		lib32-libpipewire libpipewire pipewire libpulse lib32-libpulse 
+		vkd3d lib32-vkd3d vulkan-mesa-layers lib32-vulkan-mesa-layers 
+		freetype2 lib32-freetype2 fuse2 wget yad mangohud lib32-mangohud 
+		gamescope gamemode zenity-gtk3 steam-screensaver-fix
 	)
 
 	echo '== checking for updates'
